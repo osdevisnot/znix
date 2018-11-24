73c7dad23d2d473fbd5457f2324f76654dfe5b67
osdevisnot <osdevisnot@gmail.com>
2018-11-24 01:23:50 -0800

    publish a minified version and add example usage

--- packages/znix/rollup.config.js
+++ packages/znix/rollup.config.js
@@ -2,5 +2,6 @@ const bundles = require('tslib-cli');
 
 export default bundles([
   { input: 'src/znix.ts', output: { format: 'umd', file: 'dist/znix.js', name: 'znix' } },
+  { input: 'src/znix.ts', output: { format: 'umd', file: 'dist/znix.min.js', name: 'znix' }, minify: true },
   { input: 'src/devtools.ts', output: { format: 'cjs', file: 'dist/devtools.js' } }
 ]);
